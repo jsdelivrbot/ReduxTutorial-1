2c5db0f83ce306895893ef6803a5267996f0f30c
RobGThai <thisisrobg@gmail.com>
2017-01-08 07:06:17 +0700

    Init local repo.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Create a new component
+// This component should produce some HTML.
+cosnt App = function() {
+  return <div>Hi</div>;
+}
+
+// Take this component's generated HTML and put it on page.
