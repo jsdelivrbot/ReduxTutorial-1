9591ed54cf0ce240dfe3266e1c5a64f536bca945
RobGThai <thisisrobg@gmail.com>
2017-01-10 00:38:33 +0700

    Add Youtube API.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
+import secret from './_secret.js';
+
+const YOUTUBE_API_KEY = secret.API_KEY;
+// console.log(YOUTUBE_API_KEY);
+
 // This is a class of a component
 // To create an instance of this class use <App />
+
 const App = () => {
   return (
     <div>
