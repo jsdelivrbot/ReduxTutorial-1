c3b2135dacf27b1425b9f7ab4ed1fd982303618a
RobGThai <thisisrobg@gmail.com>
2017-01-10 00:29:37 +0700

    Add SearchBar.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // This is a class of a component
 // To create an instance of this class use <App />
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Render App inside an element with class name 'container'
