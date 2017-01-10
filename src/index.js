4be35ffb0fae7670c63cbf46642014eb29a4c9a3
RobGThai <thisisrobg@gmail.com>
2017-01-11 00:39:22 +0700

    Create VideoList which use videos props from App component.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 import secret from './_secret.js';
 
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
