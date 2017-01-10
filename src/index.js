53640302616c8ddc1b0012b72cec65d17d249bba
RobGThai <thisisrobg@gmail.com>
2017-01-11 00:28:14 +0700

    Use YTSearch to search Youtube. Refactor App into class.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
@@ -8,16 +9,25 @@ import secret from './_secret.js';
 const YOUTUBE_API_KEY = secret.API_KEY;
 // console.log(YOUTUBE_API_KEY);
 
+YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards' }, (data) => {
+  console.log(data);
+})
 // This is a class of a component
 // To create an instance of this class use <App />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Render App inside an element with class name 'container'
 ReactDOM.render(<App />, document.querySelector('.container'));
