778a41520b7429727b08d3b33354a569756cb16a
RobGThai <thisisrobg@gmail.com>
2017-01-15 20:45:31 +0700

    Add selectedVideo.

--- src/index.js
+++ src/index.js
@@ -17,11 +17,15 @@ class App extends Component {
     super(props);
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
 
     YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
@@ -29,7 +33,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
