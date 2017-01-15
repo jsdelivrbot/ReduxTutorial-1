fd749ffb48055669e5570edc8d6c575d1274fbd4
RobGThai <thisisrobg@gmail.com>
2017-01-16 01:11:02 +0700

    Reset App to Component.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component
 // This component should produce some HTML.
-cosnt App = function() {
-  return <div>Hi</div>;
-}
+class App extends Component {
+  render() {
+    return (
+      <div>Hi</div>
+    );
+  } 
+};
 
 // Take this component's generated HTML and put it on page.
+ReactDOM.render(<App />, document.querySelector('.container'));
